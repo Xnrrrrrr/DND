7f637fcfbff716dcedac4a945e28b05fabe7b667
Xnrrrrrrrr <Xnrrrrrrrr@gmail.com>
2024-08-09 10:16:30 -0400

    added changes to characterSheet controller added npcController added Npc.js added npcRoutes

--- backend/controllers/characterSheetController.js
+++ backend/controllers/characterSheetController.js
@@ -8,7 +8,50 @@ const { CharacterSheet } = require("../models");
  * @access	Private
  */
 const createCharacterSheet = asyncHandler(async (req, res) => {
-	// a lot of shit
+	const { skills, characterFirstName, characterLastName, ideals, bonds, religion, flaws, backgrounds, primaryClass, subClass, armorClass, currentHitPoints, temporaryHitPoints, equipment, race, inspiration, dexterity, intelligence, wisdom, charisma, strength, proficiencyBonus, savingThrows, alignment, background, features, personalityTraits, languages, exoticLanguage, age, height, weight, skin, hair, eyes, proficiencies, characterAppearance, characterBackstory, allies, organizations,   } = req.body;
+    if (
+        !skills || 
+        !characterFirstName || 
+        !characterLastName || 
+        !ideals || 
+        !bonds || 
+        !religion || 
+        !flaws || 
+        !backgrounds || 
+        !primaryClass || 
+        !subClass || 
+        !armorClass || 
+        !currentHitPoints || 
+        !temporaryHitPoints || 
+        !equipment || 
+        !race || 
+        !inspiration || 
+        !dexterity || 
+        !intelligence || 
+        !wisdom || 
+        !charisma || 
+        !strength || 
+        !proficiencyBonus || 
+        !savingThrows || 
+        !alignment || 
+        !background || 
+        !features || 
+        !personalityTraits || 
+        !languages || 
+        !exoticLanguage || 
+        !age || 
+        !height || 
+        !weight || 
+        !skin || 
+        !hair || 
+        !eyes || 
+        !proficiencies || 
+        !characterAppearance || 
+        !characterBackstory || 
+        !allies || 
+        !organizations
+    ) {
+        res.status(StatusCodes.BAD_REQUEST);// a lot of shit
 
 	// No checks needed
 
